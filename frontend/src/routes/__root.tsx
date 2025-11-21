e392cd10bcd1f15753c466a3fc672feba75ea52b
Astreon <886847+Astreon@users.noreply.github.com>
2025-11-21 16:23:24 +0100

    init frontend v2

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,47 +1,67 @@
-import type { ReactNode } from 'react'
+import type {ReactNode} from 'react'
 import {
-  Outlet,
-  createRootRoute,
-  HeadContent,
-  Scripts,
+    Outlet,
+    createRootRoute,
+    HeadContent,
+    Scripts,
 } from '@tanstack/react-router'
+import appCss from '../app.css?url'
+import {TanStackDevtools} from "@tanstack/react-devtools";
+import {TanStackRouterDevtoolsPanel} from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
-    ],
-  }),
-  component: RootComponent,
+    head: () => ({
+        meta: [
+            {
+                charSet: 'utf-8',
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            {
+                title: 'TanStack Start Starter',
+            },
+        ],
+        links: [
+            {
+                rel: 'stylesheet',
+                href: appCss
+            }
+        ],
+    }),
+    component: RootComponent,
 })
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  )
+    return (
+        <RootDocument>
+            <Outlet/>
+        </RootDocument>
+    )
 }
 
-function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
+function RootDocument({children}: Readonly<{ children: ReactNode }>) {
+    return (
+        <html>
+        <head>
+            <HeadContent/>
+        </head>
+        <body>
         {children}
-        <Scripts />
-      </body>
-    </html>
-  )
+        <TanStackDevtools
+            config={{
+                position: 'bottom-right',
+            }}
+            plugins={[
+                {
+                    name: 'Tanstack Router',
+                    render: <TanStackRouterDevtoolsPanel/>,
+                },
+            ]}
+        />
+        <Scripts/>
+        </body>
+        </html>
+    )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
+import {defineConfig} from 'vite'
+import {devtools} from "@tanstack/devtools-vite";
+import {tanstackStart} from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tanstackStart(),
-    viteReact(),
-  ],
+    server: {
+        port: 3000,
+    },
+    plugins: [
+        devtools(),
+        viteTsConfigPaths({
+            projects: ['./tsconfig.json'],
+        }),
+        tailwindcss(),
+        tanstackStart(),
+        viteReact(),
+    ],
 })
 
 export default config
