962d67f87a24b18546b72d1a28aea3c7509cd3fe
Astreon <886847+Astreon@users.noreply.github.com>
2025-11-21 19:22:32 +0100

    Init theme provider

---
+++ frontend/src/components/theme-provider.tsx
@@ -0,0 +1,14 @@
+import React, { type PropsWithChildren } from "react"
+import {
+  ThemeProvider as BaseThemeProvider,
+  type ThemeProviderProps as BaseThemeProviderProps,
+} from 'tanstack-theme-kit'
+
+export type AppThemeProviderProps = PropsWithChildren<BaseThemeProviderProps>
+
+export const AppThemeProvider: React.FC<AppThemeProviderProps> = ({
+  children,
+  ...props
+}) => {
+  return <BaseThemeProvider {...props}>{children}</BaseThemeProvider>
+}
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -5,9 +5,14 @@ import {
     HeadContent,
     Scripts,
 } from '@tanstack/react-router'
-import appCss from '../app.css?url'
 import {TanStackDevtools} from "@tanstack/react-devtools";
 import {TanStackRouterDevtoolsPanel} from "@tanstack/react-router-devtools";
+import {AppThemeProvider as ThemeProvider} from "@/components/theme-provider";
+import {cn} from "@/lib/utils.ts";
+import appCss from '../styles/globals.css?url'
+
+import {DEFAULT_THEME} from "@/lib/themes";
+import {ActiveThemeProvider} from "@/components/active-theme.tsx";
 
 export const Route = createRootRoute({
     head: () => ({
@@ -25,9 +30,21 @@ export const Route = createRootRoute({
         ],
         links: [
             {
+                rel: 'preconnect',
+                href: 'https://fonts.googleapis.com'
+            },
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.gstatic.com',
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200..800&display=swap'
+            },
+            {
                 rel: 'stylesheet',
                 href: appCss
-            }
+            },
         ],
     }),
     component: RootComponent,
@@ -42,13 +59,35 @@ function RootComponent() {
 }
 
 function RootDocument({children}: Readonly<{ children: ReactNode }>) {
+    const themeSettings = {
+        preset: ('ocean-breeze') as any,
+        scale: (DEFAULT_THEME.scale) as any,
+        radius: (DEFAULT_THEME.radius) as any,
+        contentLayout: ('full') as any
+    };
+
+    const bodyAttributes = Object.fromEntries(
+        Object.entries(themeSettings)
+            .filter(([_, value]) => value)
+            .map(([key, value]) => [`data-theme-preset-${key.replace(/([A-Z])/g, "-$1").toLowerCase()}`, value])
+    );
+
     return (
-        <html>
+        <html suppressHydrationWarning>
         <head>
             <HeadContent/>
         </head>
-        <body>
-        {children}
+        <body suppressHydrationWarning className={cn("bg-background group/layout font-sans")} {...bodyAttributes}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <ActiveThemeProvider initialTheme={themeSettings}>
+                <div className="flex flex-1 flex-col">
+                <div
+                    className="@container/main p-4 xl:group-data-[theme-content-layout=centered]/layout:container xl:group-data-[theme-content-layout=centered]/layout:mx-auto">
+                    {children}
+                </div>
+            </div>
+            </ActiveThemeProvider>
+        </ThemeProvider>
         <TanStackDevtools
             config={{
                 position: 'bottom-right',
