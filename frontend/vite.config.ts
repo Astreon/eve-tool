d395d65e34c6ecd2917ca745d19fa0aabe12a1fc
Astreon <886847+Astreon@users.noreply.github.com>
2025-11-29 23:36:31 +0100

    dependencies for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
     server: {
@@ -26,6 +27,7 @@ const config = defineConfig({
     },
     plugins: [
         devtools(),
+        nitro(),
         viteTsConfigPaths({
             projects: ['./tsconfig.json'],
         }),
